09cd33b975834d4da2c09e968599a51c4c12f675
Fuwn <contact@fuwn.me>
2022-05-17 04:17:48 -0700

    feat(meta): meta-related utilities

--- examples/meta.rs
+++ examples/meta.rs
@@ -16,27 +16,9 @@
 // Copyright (C) 2022-2022 Fuwn <contact@fuwn.me>
 // SPDX-License-Identifier: GPL-3.0-only
 
-#![deny(
-  warnings,
-  nonstandard_style,
-  unused,
-  future_incompatible,
-  rust_2018_idioms,
-  unsafe_code,
-  clippy::all,
-  clippy::nursery,
-  clippy::pedantic
-)]
-#![recursion_limit = "128"]
-
-#[cfg(feature = "ast")]
-pub mod ast;
-
-#[cfg(feature = "convert")]
-pub mod convert;
-
-#[cfg(feature = "request")]
-pub mod request;
-
-#[cfg(feature = "meta")]
-pub mod meta;
+fn main() {
+  println!(
+    "{:?}",
+    germ::meta::Meta::from_str("text/gemini; hi=2; hi2=string=2")
+  )
+}
