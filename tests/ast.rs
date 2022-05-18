d9812f8f6525397b9dd71f5295beb98d201cbd77
Fuwn <contact@fuwn.me>
2022-05-17 18:58:50 -0700

    ci(tests): add ast tests

---
+++ tests/ast.rs
@@ -0,0 +1,70 @@
+// This file is part of Germ <https://github.com/gemrest/germ>.
+// Copyright (C) 2022-2022 Fuwn <contact@fuwn.me>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// Copyright (C) 2022-2022 Fuwn <contact@fuwn.me>
+// SPDX-License-Identifier: GPL-3.0-only
+
+#[cfg(test)]
+mod test {
+  use germ::ast::{build, Node};
+
+  #[test]
+  fn build_multi_line_list_with_text() {
+    assert_eq!(
+      build("* item1\n* 2\nhi text"),
+      vec![
+        Node::List(vec!["item1".to_string(), "2".to_string()]),
+        Node::Text("hi text".to_string()),
+      ],
+    );
+  }
+
+  #[test]
+  fn build_multi_line_vec() {
+    assert_eq!(
+      build("=> /test hi\nhi there\n> hi"),
+      vec![
+        Node::Link {
+          to:   "/test".to_string(),
+          text: Some("hi".to_string()),
+        },
+        Node::Text("hi there".to_string()),
+        Node::Blockquote("hi".to_string()),
+      ],
+    );
+  }
+
+  #[test]
+  fn build_single_0th_from_vec() {
+    assert_eq!(
+      build("=> /test hi"),
+      vec![Node::Link {
+        to:   "/test".to_string(),
+        text: Some("hi".to_string()),
+      }],
+    );
+  }
+
+  #[test]
+  fn build_single_element() {
+    assert_eq!(
+      build("=> /test hi").get(0).unwrap(),
+      &Node::Link {
+        to:   "/test".to_string(),
+        text: Some("hi".to_string()),
+      },
+    );
+  }
+}
