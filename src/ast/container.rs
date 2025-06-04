cf1e555deb1aab7b9b8288f4e2ff5895bc25b0b0
Fuwn <contact@fuwn.me>
2025-06-04 10:25:17 +0000

    docs: Update copyright header year

--- src/ast/container.rs
+++ src/ast/container.rs
@@ -1,5 +1,5 @@
 // This file is part of Germ <https://github.com/gemrest/germ>.
-// Copyright (C) 2022-2022 Fuwn <contact@fuwn.me>
+// Copyright (C) 2022-2025 Fuwn <contact@fuwn.me>
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 //
-// Copyright (C) 2022-2022 Fuwn <contact@fuwn.me>
+// Copyright (C) 2022-2025 Fuwn <contact@fuwn.me>
 // SPDX-License-Identifier: GPL-3.0-only
 
 use {super::Node, std::fmt::Write};
